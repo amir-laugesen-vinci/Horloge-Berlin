c9c0d76c8dfadde85262c9563078622d1786d485
Thanh-Truong-Vinci <thanh.truong@student.vinci.be>
2024-11-08 11:04:19 +0100

    develop : ajout de la branch develop + modification du timestamp à midi

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -5,10 +5,11 @@ describe("BerlinClock function should return a matrice the TimeStamp", function(
     const main = new Main();
 
     it("should return the correct Berlin clock format for a given timestamp", function() {
-        const timestamp = 1730995452 * 1000;
+        const timestamp = 43200 * 1000;
+        const date = new Date(timestamp);
         const result = main.BerlinClock(timestamp);
 
-        console.log("yo bg");
+        console.log(date)
 
         expect(result).toBe(13);
     });
