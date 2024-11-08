194033deb03f0ef1d15aa5ea084462932e6d1634
Thanh-Truong-Vinci <thanh.truong@student.vinci.be>
2024-11-08 11:47:35 +0100

    feature/etape1 : pas validé au final, test sur 0:00 normalement validé

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -4,13 +4,9 @@ describe("BerlinClock function should return a matrice the TimeStamp", function(
 
     const main = new Main();
 
-    it("should return the correct Berlin clock format for a given timestamp", function() {
-        const timestamp = 43200 * 1000;
-        const date = new Date(timestamp);
+    it("should return the correct Berlin clock format for 0 simple minutes", function() {
+        const timestamp = new Date(Date.UTC(1970, 0, 1, 0, 0)).getTime(); // actuellement 00:00
         const result = main.BerlinClock(timestamp);
-
-        console.log(date)
-
         expect(result).toBe("RRRR");
     });
 });
