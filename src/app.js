73a60982682c569d11ddba5abf3cccc5fe0a98e8
laugesen <amir.laugesen@student.vinci.be>
2024-11-10 15:26:22 +0100

    add : rajouter tests pour minutes par 5

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 export class Main{
     BerlinClock(timestamp){
         const date = new Date(timestamp);
+
         const minutes = date.getUTCMinutes();
 
-        const simpleMinutes = minutes%5;
-        const lignesSimpleMinutes = 'O'.repeat(simpleMinutes) + 'C'.repeat(4-simpleMinutes);
+        console.log("const date : ", date);
+        console.log("les minutes : ", minutes);
 
-        return lignesSimpleMinutes;
-        
+        const simpleMinutes = minutes%5;
+        const lignesSimpleMinutes = 'O'.repeat(simpleMinutes) + 'C'.repeat(4 - simpleMinutes);
+
+        //return lignesSimpleMinutes;
+
+        const MinutesPar5 = Math.floor(minutes / 5);
+        const lignesMinutesPar5 = 'O'.repeat(MinutesPar5) + 'C'.repeat(11 - MinutesPar5);
+
+        console.log("lignes simple minutes: ", lignesSimpleMinutes);
+        console.log("lignes minutes par 5: ", lignesMinutesPar5);
+
+        return lignesMinutesPar5;
     }
 }
