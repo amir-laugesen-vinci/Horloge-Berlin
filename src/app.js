2939123dfc07a841abc0f8b6e5c142f744a7e84e
laugesen <amir.laugesen@student.vinci.be>
2024-11-10 17:38:33 +0100

    feature/etape4 : add : rajout code pour gerer les heures par 5

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ export class Main {
     BerlinClock(timestamp) {
         const date = new Date(timestamp);
         
-        const hours = date.getUTCHours();
+
         const minutes = date.getUTCMinutes();
+        const hours = date.getUTCHours();
+
 
         const simpleMinutes = minutes % 5;
         const minutesPar5 = Math.floor(minutes / 5);
-
-        const lignesMinutesPar5 = 'O'.repeat(minutesPar5) + 'C'.repeat(11 - minutesPar5);
-        const lignesSimpleMinutes = 'O'.repeat(simpleMinutes) + 'C'.repeat(4 - simpleMinutes);
-
         const simpleHours = hours % 5;
+        const heuresPar5 = Math.floor(hours / 5);
+
+        const lignesSimpleMinutes = 'O'.repeat(simpleMinutes) + 'C'.repeat(4 - simpleMinutes);
+        const lignesMinutesPar5 = 'O'.repeat(minutesPar5) + 'C'.repeat(11 - minutesPar5);
         const lignesSimpleHours = 'O'.repeat(simpleHours) + 'C'.repeat(4 - simpleHours);
-        return lignesSimpleMinutes + "\n" + lignesMinutesPar5 + "\n" + lignesSimpleHours;
+        const lignesHeurePar5 = 'O'.repeat(heuresPar5) + 'C'.repeat(4 - heuresPar5);
+
+
+        return lignesSimpleMinutes + "\n" + lignesMinutesPar5 + "\n" + lignesSimpleHours + "\n" + lignesHeuresPar5;
+
+
     }
 }
